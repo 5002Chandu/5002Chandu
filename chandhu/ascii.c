be070270008d93cdb403088a7619310949c0aa35
5002Chandu <chanducharan2006@gmail.com>
2024-12-09 16:04:44 +0530

    Add files via upload
    09-12-24

---
+++ chandhu/ascii.c
@@ -0,0 +1,16 @@
+//#include<stdio.h>
+//int main()
+//{
+    //char ch='a';
+   // printf("The ascii value of '%c' is %d\n",ch,ch);
+  //return 0;
+//}
+
+#include<stdio.h>
+int main()
+{
+    int ascii_value=65;
+    char ch=(char)ascii_value;
+    printf("The character corresponding of ASCII value %d is '%c'/n", ascii_value,ch);
+    return 0;
+}
