79364737b2b8b85fb6dec94e9edf1cf473f042e5
unknown <chanducharan2006@gmail.com>
2024-12-10 11:46:33 +0530

    3rd assignment

--- chandhu/array.c
+++ chandhu/array.c
@@ -7,26 +7,26 @@ int main()
     {
         scanf("%d",&arr[i]);
     }
-    // printf("The array is:[");
-    // for (int i=0;i<5;i++)
-    // {
-    //     if(i<4)
-    //     {
-    //         printf("%d,",arr[i]);
-    //     }
-    //     else
-    //     {
-    //         printf("%d",arr[i]);
-    //     }
-    // }
+     printf("The array is:[");
+     for (int i=0;i<5;i++)
+    {
+         if(i<4)
+        {
+             printf("%d,",arr[i]);
+         }
+        else
+        {
+            printf("%d",arr[i]);
+        }
+     }
 
-    // printf("]\n");
-    // int sum=0;
-    // for(int i=0;i<5;i++)
-    // {
-    //     sum += arr[i];
-    // }
-    // printf("The sum of array elements is :%d",sum);
+     printf("]\n");
+     int sum=0;
+     for(int i=0;i<5;i++)
+     {
+         sum += arr[i];
+    }
+     printf("The sum of array elements is :%d",sum);
     return 0;
 }
 
